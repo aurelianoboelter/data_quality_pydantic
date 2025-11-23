52274b18a87c5dc64671e870df2cdcdc49e50828
Aurelio <aurelio.boelter@gmail.com>
2025-11-23 15:58:01 -0300

    atualizando

--- dags/dim_institucional/exampledag.py
+++ dags/dim_institucional/exampledag.py
@@ -4,7 +4,7 @@ from airflow.decorators import dag, task
 
 
 @dag(
-    dag_id="exemplo_taskflow",
+    dag_id="exemplo_taskflow_01",
     start_date=datetime(2025, 10, 13),
     schedule=None,
     catchup=False,
@@ -22,7 +22,7 @@ def exemplo_taskflow_dag():
 
     @task()
     def carregar_dados(dados_transformados):
-        print(f"Carregando dados: {dados_transformados}")
+        print(f"Carregando dados : {dados_transformados}")
 
     # Definindo dependências automaticamente
     dados = extrair_dados()
