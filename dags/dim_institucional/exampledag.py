b02247c795eb3bc42f8d91596186c3b5207e2f95
Aurelio <aurelio.boelter@gmail.com>
2025-11-25 16:08:03 -0300

    Teste com alexandre

--- dags/dim_institucional/exampledag.py
+++ dags/dim_institucional/exampledag.py
@@ -4,7 +4,7 @@ from airflow.decorators import dag, task
 
 
 @dag(
-    dag_id="exemplo_taskflow_01",
+    dag_id="exemplo_taskflow",
     start_date=datetime(2025, 10, 13),
     schedule=None,
     catchup=False,
